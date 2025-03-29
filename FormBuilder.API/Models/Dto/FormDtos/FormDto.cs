bfee0103095f01e26be155967e712f453952c4bf
augustofrade <aquaaticnaotrava@gmail.com>
2025-03-28 22:14:43 -0300

    Add basic Form, Question and QuestionOption Dtos

---
+++ FormBuilder.API/Models/Dto/FormDtos/FormDto.cs
@@ -0,0 +1,10 @@
+namespace FormBuilder.API.Models.Dto.FormDtos;
+
+public class FormDto
+{
+    public Guid Id { get; init; }
+    public string PublicId { get; set; }
+    public string Title { get; init; }
+    public string Description { get; init; }
+    public DateTime CreatedAt { get; init; }
+}
---
+++ FormBuilder.API/Models/Dto/FormDtos/QuestionOptionDto.cs
@@ -0,0 +1,8 @@
+namespace FormBuilder.API.Models.Dto.FormDtos;
+
+public class QuestionOptionDto
+{
+    public Guid Id { get; init; }
+    public string Value { get; init; }
+    public string Label { get; init; }
+}
