09cc6ae491f07aa30ecdc8885097d1acc0647354
augustofrade <aquaaticnaotrava@gmail.com>
2025-04-19 17:57:21 -0300

    Add Form Submission details dto

---
+++ FormBuilder.API/Models/Dto/FormDtos/FormSubmission/FormSubmissionDto.cs
@@ -0,0 +1,5 @@
+using System.Collections;
+
+namespace FormBuilder.API.Models.Dto.FormDtos.FormSubmission;
+
+public record FormSubmissionDto(DateTime SubmittedOn, IEnumerable<QuestionAnswerDto> Answers);
