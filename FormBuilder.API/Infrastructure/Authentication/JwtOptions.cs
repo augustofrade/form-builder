21e055c9053ea7ec103f04c4a3cb36c52e28ceb6
augustofrade <aquaaticnaotrava@gmail.com>
2025-04-22 22:34:40 -0300

    Add JwtOptions class and appsettings configuration

---
+++ FormBuilder.API/Infrastructure/Authentication/JwtOptions.cs
@@ -0,0 +1,9 @@
+namespace FormBuilder.API.Infrastructure.Authentication;
+
+public class JwtOptions
+{
+    public string PrivateKey { get; init; }
+    public string Issuer { get; init; }
+    public string Audience { get; init; }
+    public int ExpirationMinutes { get; init; }
+}
